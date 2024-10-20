db1f7bb01f9c9da7fa846b47fea26a5646af31f2
nazwa121 <nazwahafizah2305@gmail.com>
2024-10-20 14:35:05 +0700

    menambahkan jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,25 @@
+# beberapa tipe data yang ada di python 
+# 1. string
+# string digunakan untuk menyimpan data berupa huruf, simbol, dan karakter lain, string menggunakan simbol quote atau double quote yang membungkus nilai di dalamnya
+nama = "nazwa hafizah tanjung"
+print(nama)
+print(type(nama))
+
+# 2. integer 
+# integer digunakan untuk data berupa angka desimal 0,1,2...
+umur= 18
+print(umur)
+print(type(umur))
+
+# 3. float
+# float digunakan untuk data berupa angka dengan pecahan seperti contohnya 22.1
+phi= 3.14
+print(phi)
+print(type(phi))
+
+# 4. boolean 
+# boolean adalah tipe data yang digunakan untuk mempresentasikan nilai kebenaran. Data ini akan erat kaitannya dengan logika matematika, dimana datanya hanya akan terdiri dari dua jenis, yaitu TRUE dan FALSE.
+x = 5
+y = 10
+print(x<y)
+print(type(x<y))
